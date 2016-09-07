84dbd842c53ef426a1d85db106b6105390f6ae65
jcchv <jcchv92@gmail.com>
2016-09-06 21:18:00 -0600

    Depuracion
    Depuracion

--- js/owl.js
+++ js/owl.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
 
-	$("#owl-demo").owlCarousel({
-		items : 2,
-		lazyLoad : true,
-		navigation : true,
-		itemsDesktop: false,
+  $("#mainOwl").owlCarousel({
+    items : 1,
+    lazyLoad : true,
+    navigation : true,
+    itemsDesktop: false,
+      itemsDesktop : false, //5 items between 1000px and 901px
+        itemsDesktopSmall : false, // betweem 900px and 601px
+        itemsTablet: false,
+        autoPlay : true,
+        slideSpeed : 200,
+        stopOnHover: true,
+        navigation : false
+
+      });
+
+  $("#owl-demo").owlCarousel({
+    items : 2,
+    lazyLoad : true,
+    navigation : true,
+    itemsDesktop: false,
     	itemsDesktop : false, //5 items between 1000px and 901px
       	itemsDesktopSmall : false, // betweem 900px and 601px
       	itemsTablet: false,
@@ -14,11 +29,11 @@ $(document).ready(function() {
       	navigation : false
       }); 
 
-	$("#owl").owlCarousel({
-		items : 3,
-		lazyLoad : true,
-		navigation : true,
-		itemsDesktop: false,
+  $("#owl").owlCarousel({
+    items : 3,
+    lazyLoad : true,
+    navigation : true,
+    itemsDesktop: false,
     	itemsDesktop : false, //5 items between 1000px and 901px
       	itemsDesktopSmall : false, // betweem 900px and 601px
       	itemsTablet: false,
