f01cf6484ca5baed0314f44910cf1b2afe882dea
jcchv <jcchv92@gmail.com>
2016-09-16 01:31:19 -0600

    Commit End
    ENd

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,14 @@
-$(document).ready(function() {
-  $('a[href^=""]').click(function(e){
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: 0 }, 'fast');
-  });
-};
+   $('a[href^=""]').click(function(e){
+   	e.preventDefault();
+   	$('html, body').animate({ scrollTop: 0 }, 'fast');
+   });
+   $('.hidenav').click(function(e){
+
+   	e.preventDefault();
+   	$('html, body').animate({ scrollTop: 0 }, 'fast');
+
+   	
+   });
+   $('.navbar-collapse a').click(function(){
+   	$(".navbar-collapse").collapse('hide');
+   });
